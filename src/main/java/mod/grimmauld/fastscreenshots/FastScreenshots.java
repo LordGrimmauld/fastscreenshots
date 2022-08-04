b42d0540f3913e4224e31da86b0f70618f709dbc
Grimmauld <soeren@benjos.de>
2022-08-04 18:56:37 +0200

    Initial commit

---
+++ src/main/java/mod/grimmauld/fastscreenshots/FastScreenshots.java
@@ -0,0 +1,13 @@
+package mod.grimmauld.fastscreenshots;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(BuildConfig.MODID)
+public class FastScreenshots {
+	private static final Logger LOGGER = LogManager.getLogger(BuildConfig.MODID);
+
+	public FastScreenshots() {
+	}
+}
